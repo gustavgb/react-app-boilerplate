e3954de71c5fed0d9d030d13b2eed3ca820074e5
gustavgb <gustavburchardt@gmail.com>
2018-03-09 22:22:51 +0100

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'features/app/container';
-import registerServiceWorker from './registerServiceWorker';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import registerServiceWorker from 'registerServiceWorker';
+import store, { history } from 'store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+// eslint-disable-next-line no-undef
+, document.getElementById('root'));
 registerServiceWorker();
